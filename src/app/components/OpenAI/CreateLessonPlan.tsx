5a83b93a2bd6c0bf0be5bd113094f733c1539b13
Soroush Mirzaee <DeltaTheM2@users.noreply.github.com>
2024-03-27 01:34:22 -0700

    update in create and run

--- src/app/components/OpenAI/CreateLessonPlan.tsx
+++ src/app/components/OpenAI/CreateLessonPlan.tsx
@@ -8,15 +8,13 @@ const openai = new OpenAI();
 const assistant_id:string = 'asst_tN6vVKkXrNn0m6g4XOsRFw0V';
 export  default async function CreateLessonPlan(message: string) {
             const myAssistant = await openai.beta.assistants.retrieve(assistant_id);
-            const myThread = await openai.beta.threads.create();
-            const newMessage = openai.beta.threads.messages.create(
-                myThread.id,
-                {role: 'user',content: message}
-            );
-            const newRun = await openai.beta.threads.create(
-                myThread,
-                {assistant_id: assistant_id}    
-            );
+            const run = await openai.beta.threads.createAndRun({assistant_id: assistant_id,
+            thread:{
+                messages: [
+                    {role: 'user', content: message}
+                ]
+            }});
+            
             
             
     }
