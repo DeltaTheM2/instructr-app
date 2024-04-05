494a3e6ff4b133891dda2a0fb4693e71ce8760a6
Soroush Mirzaee <DeltaTheM2@users.noreply.github.com>
2024-04-05 01:39:36 -0700

    weekdaySelection added

--- src/app/components/newLessonForm/newLessonForm.tsx
+++ src/app/components/newLessonForm/newLessonForm.tsx
@@ -1,28 +1,41 @@
 ﻿import React from "react";
 import {Input, Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, useDisclosure, Checkbox, Button, Link} from "@nextui-org/react";
+import {Textarea} from "@nextui-org/input";
+import WeekdaySelection from "@/app/components/weekdaySelection/weekdaySelection";
 
 
 const NewLessonForm = ({ isOpen, onOpen, onOpenChange }: { isOpen: any; onOpen: any, onOpenChange: any}) => {
     return (
        <>
-        <ModalContent>
-            {(onClose) =>(
-                <>
-                    <ModalHeader className={'flex flex-col gap-1'}>Set up a lesson plan</ModalHeader>
-                    <ModalBody>
-                        <Input
-                            autoFocus
-                            label='Course Name'
-                            placeholder={'Enter the name'}
-                            variant={'bordered'}
-                        />
-                        <Input
-                            label={''}
-                        />
-                    </ModalBody>
-                </>
-            )}
-        </ModalContent>
+           <Modal
+               isOpen={isOpen}
+               onOpenChange={onOpenChange}
+               placement={"bottom"}
+               backdrop={"blur"}
+               size={'5xl'}
+           className={'absolute inset-y-0 bottom-0'}>
+                <ModalContent>
+                    {(onClose) =>(
+                        <>
+                            <ModalHeader className={'flex flex-col gap-1'}>Set up a lesson plan</ModalHeader>
+                            <ModalBody>
+                                <Input
+                                    autoFocus
+                                    label='Course Name'
+                                    placeholder={'Enter the name'}
+                                    variant={'bordered'}
+                                />
+                                <Textarea
+                                    label={'Description'}
+                                    size={"lg"}
+                                    variant={"bordered"}
+                                />
+                                <WeekdaySelection></WeekdaySelection>
+                            </ModalBody>
+                        </>
+                    )}
+                </ModalContent>
+           </Modal>
        </>
     );
 };
---
+++ src/app/components/weekdaySelection/weekdaySelection.tsx
@@ -0,0 +1,29 @@
+﻿import React from "react";
+import {Select, SelectItem} from "@nextui-org/react";
+
+const weekdaySelection: React.FC = () => {
+    const data =[
+        {label:'Monday', value:'monday'},
+        {label:'Tuesday', value:'tuesday'},
+        {label:'Wednesday', value:'wednesday'},
+        {label:'Thursday', value:'thursday'},
+        {label:'Friday', value:'friday'},
+        {label:'Saturday', value:'Saturday'},
+        {label:'Sunday', value:'sunday'}
+    ];
+    return (
+        <Select
+            label="Class Days"
+            placeholder="Select Class day"
+            selectionMode="multiple"
+            className="max-w-xs"
+        >
+            {data.map((day) => (
+                <SelectItem key={day.value} value={day.value}>
+                    {day.label}
+                </SelectItem>
+            ))}
+        </Select>
+    );
+}
+export default weekdaySelection;
